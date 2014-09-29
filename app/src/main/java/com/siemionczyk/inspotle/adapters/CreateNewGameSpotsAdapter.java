7b6e97e563a310818ab9f6af9532954d33cef956
Michał Siemiończyk <michal.siemionczyk@elpassion.pl>
2014-09-29 22:37:09 +0200

    fixed bug in displaying nr activities in Create New Game view

--- app/src/main/java/com/siemionczyk/inspotle/adapters/CreateNewGameSpotsAdapter.java
+++ app/src/main/java/com/siemionczyk/inspotle/adapters/CreateNewGameSpotsAdapter.java
@@ -1,7 +1,6 @@
 package com.siemionczyk.inspotle.adapters;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.siemionczyk.inspotle.R;
 import com.siemionczyk.inspotle.model.Activity;
@@ -50,71 +50,75 @@ public class CreateNewGameSpotsAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        convertView = createIfNeeded(convertView, parent);
-        bind(convertView, getItem(position));
-        return convertView;
-    }
 
-    private View createIfNeeded(View convertView, ViewGroup parent) {
+        Viewholder viewholder = null;
+        Spot spot = getItem(position);
+
         if (convertView == null) {
             convertView = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.create_new_game_spot_item, parent, false);
+            viewholder = createAndConnectViewholder(convertView, spot);
+        } else {
+            viewholder = (Viewholder) convertView.getTag();
         }
+
+        bind(viewholder, spot);
         return convertView;
     }
 
-    static int  id = 45435433;
+    private Viewholder createAndConnectViewholder(View convertView, Spot spot) {
+        Viewholder viewholder;
+        viewholder = new Viewholder();
+        viewholder.photosLayout = ViewUtils.findView(convertView, R.id.layout_photos_activities);
+        viewholder.spotName = ViewUtils.findView(convertView, R.id.tv_spot_name);
+        viewholder.tvDistanceFrom = ViewUtils.findView(convertView, R.id.tv_distance_from);
+        viewholder.tvEventsNr = ViewUtils.findView(convertView, R.id.tv_events_nr);
+        viewholder.ivPhotos = getInitializedArray(context, spot.getActivities().size());
+        convertView.setTag(viewholder);
+        return viewholder;
+    }
+
+    private void injectImageViewsToLayout(LinearLayout photosLayout, ImageView[] ivPhotos) {
+        for (ImageView imageView : ivPhotos) {
+            photosLayout.addView(imageView);
+        }
+    }
 
 
-    private void bind(View view, Spot spot) {
-        ViewUtils.setText(spot.getName(), view, R.id.tv_spot_name);
-        ViewUtils.setText(spot.getLatitude() + " " + spot.getLongitude(), view, R.id.tv_distance_from);
-        ViewUtils.setText(getNrEvents(spot.getEvents().size()), view, R.id.tv_events_nr);
 
+    private ImageView[] getInitializedArray(Context ctx, int nrImages) {
+        ImageView[] ivArray = new ImageView[nrImages];
+        for (int i = 0; i < ivArray.length; i++) {
+            ivArray[i] = new ImageView(ctx);
+        }
+        return ivArray;
+    }
 
-        LinearLayout photosLayout = ViewUtils.findView(view, R.id.layout_photos_activities);
+    private void bind(Viewholder viewHolder, Spot spot) {
+        viewHolder.tvEventsNr.setText("" + spot.getEvents().size());
+        viewHolder.tvDistanceFrom.setText(spot.getLatitude() + " " + spot.getLongitude());
+        viewHolder.spotName.setText(spot.getName());
 
         Log.i(TAG, "name: " + spot.getName() + ", activieis:" + spot.getActivities().size());
 
-        for (Activity activity : spot.getActivities()) {
+        viewHolder.photosLayout.removeAllViews();
+        for (int i = 0; i < spot.getActivities().size(); i++) {
             ImageView iv = new ImageView(context);
             LinearLayout.LayoutParams params =
                     new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             params.setMargins(15, 0, 15, 0);
             iv.setLayoutParams(params);
 
-
-//            iv.setId(id);
-
-            ViewUtils.addView(photosLayout, iv);
+            ViewUtils.addView(viewHolder.photosLayout, iv);
+            Activity activity = spot.getActivities().get(i);
             Picasso.with(context).load(activity.getIcon_blue_url()).into(iv);
-            id++;
         }
     }
 
-
-    private void addPhotos() {
-
-
-//        ImageView iv = new ImageView( getActivity());
-//        LinearLayout.LayoutParams params =
-//                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        params.setMargins( 15, 0, 15, 0 );
-//
-//
-//        iv.setLayoutParams( params);
-//        iv.setId(id );
-//
-//
-//
-//
-//        photosLayout.addView( iv);
-//
-//        aq.id( id).image( OtwarteZabytkiClient.HOST + pJ.file.midi.url);
-    }
-
-
-    private String getNrEvents(int nrEvents) {
-        Resources res = this.context.getResources();
-        return res.getQuantityString(R.plurals.numberOfEvents, nrEvents, nrEvents);
+    private static class Viewholder {
+        TextView spotName;  //tv_spot_name
+        TextView tvDistanceFrom; //tv_distance_from
+        TextView tvEventsNr; //tv_events_nr
+        ImageView[] ivPhotos;
+        LinearLayout photosLayout; //layout_photos_activities
     }
 }
